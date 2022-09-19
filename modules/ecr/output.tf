53594197aa17b16b77e78ae3d929971fd2da00af
samrdaymond <106212725+samrdaymond@users.noreply.github.com>
2022-09-19 12:49:21 +1000

    moved back ecr files and fixed location of output command

--- modules/ecr/output.tf
+++ modules/ecr/output.tf
@@ -2,3 +2,7 @@ output "ecr_repo_url" {
   description = "URL of the ECR repo"
   value       = aws_ecr_repository.ecr.repository_url
 }
+
+output "ecr_name" {
+  value = aws_ecr_repository.samrdaymond_wa_repo.name
+}
